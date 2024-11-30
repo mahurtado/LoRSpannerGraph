068f290529d6307fa471b39b78eef1bee22a0ba1
Manuel Hurtado <manuelhurtado@google.com>
2024-11-30 21:23:22 +0100

    v3

--- infra-lor/network/main.tf
+++ infra-lor/network/main.tf
@@ -8,4 +8,5 @@ resource "google_compute_subnetwork" "lor-network-region" {
   ip_cidr_range = "10.2.0.0/16"
   region        = var.region
   network       = google_compute_network.lor-network.id
+  private_ip_google_access = true
 }
